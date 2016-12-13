d7dff2f7bbd70de8aea9f8c502d4a1e6504f5bfc
zhuangsheng <zhuang3c@gmail.com>
2016-12-13 22:24:27 +0800

    doublyLinkedList bug fixed

--- list/doublyLinkedList.go
+++ list/doublyLinkedList.go
@@ -40,17 +40,24 @@ func (d *DoublyLinkedList) Insert(position int, element interface{}) error {
 	node.Element = element
 
 	if position == 0 {
-		node.Next = d.head
-		if d.head != nil {
+		if d.head == nil {
+			d.tail = &node
+		} else {
 			d.head.Prev = &node
 		}
+		node.Next = d.head
 		d.head = &node
+		d.length++
 		return nil
 	}
+
 	if position == d.length {
 		node.Prev = d.tail
-		d.tail.Next = &node
+		if d.tail != nil {
+			d.tail.Next = &node
+		}
 		d.tail = &node
+		d.length++
 		return nil
 	}
 	current := d.head.Next
@@ -64,10 +71,61 @@ func (d *DoublyLinkedList) Insert(position int, element interface{}) error {
 			node.Next = current
 			current.Prev.Next = &node
 			current.Prev = &node
+			d.length++
 			return nil
 		}
+		index++
 		current = current.Next
 	}
-	d.length++
 	return nil
+}
+
+//RemoveAt ...
+func (d *DoublyLinkedList) RemoveAt(position int) (interface{}, error) {
+	if position < 0 || position >= d.length {
+		return nil, errors.New("position out of range")
+	}
+	var item interface{}
+	if position == 0 {
+		item = d.head.Element
+		d.head = d.head.Next
+		if d.head != nil {
+			d.head.Prev = nil
+		}
+		if position == 1 {
+			d.tail = d.head
+		}
+		d.length--
+		return item, nil
+	}
+	if position == d.length {
+		item = d.tail.Element
+		d.tail = d.tail.Prev
+		d.tail.Next = nil
+		if position == 1 {
+			d.head = d.tail
+		}
+		d.length--
+		return item, nil
+	}
+
+	current := d.head.Next
+	index := 1
+
+	for {
+		if current == nil {
+			break
+		}
+
+		if position == index {
+			item = current.Element
+			current.Prev.Next = current.Next
+			current.Next.Prev = current.Prev
+			break
+		}
+		current = current.Next
+		index++
+	}
+	d.length--
+	return item, nil
 }
--- list/doublyLinkedList_test.go
+++ list/doublyLinkedList_test.go
@@ -1,14 +1,13 @@
 package list
 
-import (
-	"testing"
-)
+import "testing"
 
 func TestDubltyLinkedListInsert(t *testing.T) {
 	d := DoublyLinkedList{}
 	d.Insert(0, "99")
 	d.Insert(1, "88")
 	d.Insert(1, 100)
+	d.Insert(2, 101)
 
 	current := d.head
 	index := 0
@@ -22,9 +21,43 @@ func TestDubltyLinkedListInsert(t *testing.T) {
 		if index == 1 && current.Element != 100 {
 			t.Errorf("index %d get %s expect int 100 ", index, current.Element)
 		}
-		if index == 2 && current.Element != "88" {
+		if index == 2 && current.Element != 101 {
+			t.Errorf("index %d get %s expect string 101 ", index, current.Element)
+		}
+		index++
+		current = current.Next
+	}
+}
+
+func TestDubltyLinkedListRemoveAt(t *testing.T) {
+	d := DoublyLinkedList{}
+	d.Insert(0, "99")
+	d.Insert(1, "88")
+	d.Insert(1, 100)
+	d.Insert(2, 101)
+
+	del, err := d.RemoveAt(1)
+	if del != 100 {
+		t.Errorf("remove index 1 expect 100 get %#v err: %#v \n", del, err)
+	}
+
+	del, err = d.RemoveAt(0)
+	if del != "99" {
+		t.Errorf("remove index 1 expect 99 get %d err: %#v \n", del, err)
+	}
+	current := d.head
+	index := 0
+	for {
+		if current == nil {
+			break
+		}
+		if index == 0 && current.Element != 101 {
+			t.Errorf("index %d get %s expect int 101 ", index, current.Element)
+		}
+		if index == 1 && current.Element != "88" {
 			t.Errorf("index %d get %s expect string 88 ", index, current.Element)
 		}
+		index++
 		current = current.Next
 	}
 }
