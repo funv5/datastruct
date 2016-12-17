652b54649de9fba22dcced64121bc7c78d5ace3a
zhuangsheng <zhuang3c@gmail.com>
2016-12-17 23:04:49 +0800

    hash table conflict

--- hash/hashTable.go
+++ hash/hashTable.go
@@ -1,8 +1,12 @@
 package hash
 
+import (
+	"datastruct/list"
+)
+
 // HashTable ...
 type HashTable struct {
-	table map[int]interface{}
+	table map[int]*list.LinkedList
 }
 
 func loseloseHashCode(key string) int {
@@ -13,20 +17,61 @@ func loseloseHashCode(key string) int {
 	return hash % 37
 }
 
+type valuePair struct {
+	Key   string
+	Value interface{}
+}
+
 // Put ...
 func (h *HashTable) Put(key string, value interface{}) {
 	if h.table == nil {
-		h.table = make(map[int]interface{})
+		h.table = make(map[int]*list.LinkedList)
 	}
-	h.table[loseloseHashCode(key)] = value
+	position := loseloseHashCode(key)
+	vp := valuePair{}
+	vp.Key = key
+	vp.Value = value
+	if _, ok := h.table[position]; !ok {
+		h.table[position] = &list.LinkedList{}
+	}
+	h.table[position].Append(vp)
 }
 
 // Remove ...
 func (h *HashTable) Remove(key string) {
-	delete(h.table, loseloseHashCode(key))
+	position := loseloseHashCode(key)
+	if _, ok := h.table[position]; ok {
+		vp := valuePair{}
+		vp.Key = key
+		current := h.table[position].Head()
+		for {
+			if current == nil {
+				break
+			}
+			v, r := current.Element.(valuePair)
+			if r && v.Key == key {
+				h.table[position].Remove(v)
+			}
+			current = current.Next
+		}
+	}
 }
 
 // Get ...
 func (h *HashTable) Get(key string) interface{} {
-	return h.table[loseloseHashCode(key)]
+	position := loseloseHashCode(key)
+	if _, ok := h.table[position]; ok {
+		current := h.table[position].Head()
+		for {
+			if current == nil {
+				break
+			}
+			v, r := current.Element.(valuePair)
+			if r && v.Key == key {
+				return v.Value
+			}
+			current = current.Next
+		}
+	}
+	return nil
 }
--- hash/hashTable_test.go
+++ hash/hashTable_test.go
@@ -6,19 +6,25 @@ import (
 
 func TestPut(t *testing.T) {
 	h := HashTable{}
-	h.Put("abc", "99")
-	v := h.Get("abc")
+	h.Put("Mindy", "99")
+	h.Put("Paul", "100")
+	v := h.Get("Mindy")
 	if v != "99" {
+		t.Errorf("key abc get %s expect %s \n", v, "99")
+	}
+	v = h.Get("Paul")
+	if v != "100" {
 		t.Errorf("key abc get %s expect %s \n", v, "99")
 	}
 }
 
 func TestRemove(t *testing.T) {
 	h := HashTable{}
-	h.Put("abc", "99")
-	h.Remove("abc")
-	v := h.Get("abc")
-	if v == "99" {
+	h.Put("Mindy", "99")
+	h.Put("Paul", "100")
+	h.Remove("Paul")
+	v := h.Get("Mindy")
+	if v != "99" {
 		t.Errorf("key abc get %s expect %s \n", v, "")
 	}
 }
